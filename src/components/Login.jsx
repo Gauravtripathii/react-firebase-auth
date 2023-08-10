902357feee367d28002e17fc3f9be1fa264a29c9
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-08-10 17:03:20 +0530

    auth page basic setup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+  
+    const handleLogin = async () => {};
+  
+    return (
+      <div>
+        <h1>Login</h1>
+        <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+        <button onClick={handleLogin}>Signup</button>
+        <span>
+          Don't an account?
+          <Link to="/signup">Signup</Link>
+        </span>
+      </div>
+    );
 }
 
-export default Login
+export default Login;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSignup = async () => {};
+
   return (
-    <div>Signup</div>
-  )
+    <div>
+      <h1>Signup</h1>
+      <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+      <button onClick={handleSignup}>Signup</button>
+      <span>
+        Already have an account?
+        <Link to="/login">Login</Link>
+      </span>
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
