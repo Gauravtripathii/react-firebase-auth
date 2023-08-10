f2fe751b25eedc897f5804e31719698fc6b1d227
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-08-10 18:21:21 +0530

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { firebaseAuth } from "../firebase-config";
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
   
-    const handleLogin = async () => {};
+    const handleLogin = async () => {
+      try {
+        signInWithEmailAndPassword(firebaseAuth, email, password);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    onAuthStateChanged(firebaseAuth, (currentUser)=>{
+      if (currentUser) navigate("/");
+    })
   
     return (
       <div>
         <h1>Login</h1>
         <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
         <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-        <button onClick={handleLogin}>Signup</button>
+        <button onClick={handleLogin}>Login</button>
         <span>
           Don't an account?
           <Link to="/signup">Signup</Link>
