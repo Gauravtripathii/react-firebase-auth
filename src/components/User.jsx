eea16af7ece4931950608c535a7ff2de22696493
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-08-10 18:17:50 +0530

    user page

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import { firebaseAuth } from "../firebase-config";
 
 function User() {
+  const [user, setUser] = useState(undefined);
+  const navigate = useNavigate();
+
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) setUser(currentUser);
+    else navigate("/login");
+  });
+
   return (
     <div>
-      <h1>Welcome, </h1>
-      <button>SignOut</button>
+      <h1>Welcome, {user?.email}!</h1>
+      <button onClick={() => signOut(firebaseAuth)}>SignOut</button>
     </div>
   );
 }
